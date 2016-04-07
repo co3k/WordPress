bc3605139bd145d91a5f47495374cd8ee02ec9b9
Kousuke Ebihara <Kosuke_Ebihara@voyagegroup.com>
2016-04-07 12:35:09 +0900

    added ability to set default language configuration

--- wp-config.php
+++ wp-config.php
@@ -89,5 +89,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
     $_SERVER['HTTPS'] = 'on';
 }
 
+define('WPLANG', getenv('WP_LANG'));
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
